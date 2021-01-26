76decb23870ab9b6da0fef7d4dd15c0b287cc465
Jonathan <quezako35@gmail.com>
2021-01-26 08:43:46 +0700

    My subs: manually added my subs, playlists and filters.

--- yt-my-subs.php
+++ yt-my-subs.php
@@ -1,15 +1,33 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+session_start();
+
+header('Pragma: public'); 	// required
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+// header('Content-Type: application/force-download');
+// header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+header('Content-Disposition: attachment; filename="yt-my-subs.php.html"');
+header('Expires: 0');
+// header('Cache-Control: must-revalidate');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Cache-Control: private', false);
+header('Content-Transfer-Encoding: binary');
+// header('Content-Length: ' . filesize($filepath));
+// header('Connection: close');
+flush(); // Flush system output buffer
+// readfile($filepath);
+//die();
+ini_set('max_execution_time', 600); //300 seconds = 5 minutes
+set_time_limit(600);
+
 
 // Call set_include_path() as needed to point to your client library.
 require_once 'config.php';
-//require_once 'src/Google/autoload.php';
-//require_once 'src/Google/Client.php';
-//require_once 'src/Google/Service/YouTube.php';
 require_once 'vendor/autoload.php';
 
-session_start();
+echo '<meta charset="UTF-8"><pre>';
 
 // SQLite.
 try {
@@ -76,10 +94,8 @@ if (isset($_GET['code']) && ! isset($dbToken)) {
     }
     
     $dbToken = $client->getAccessToken();
-    //echo '<pre>';var_dump($dbToken);die;
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array(
-        //':token' => $dbToken,
         ':token' => json_encode($dbToken),
         ':type' => 'token'
     ));
@@ -127,137 +143,253 @@ if ($client->getAccessToken()) {
     $videoIds = isset($_GET['video_ids']) ? $_GET['video_ids'] : '';
     $playlistId = isset($_GET['playlist_id']) ? $_GET['playlist_id'] : '';
     // Define an object that will be used to make all API requests.
-    $youtube = new Google_Service_YouTube($client);
-    
-    // Get all the playlists and generate a select list.
-    $playlistsResponse = $youtube->playlists->listPlaylists('snippet', [
-        'mine' => 'true',
-        'maxResults' => 50
-    ]);
-    
-    // Get order from DB.
-    $stmt = $pdo->prepare("SELECT * FROM playlists");
-    $stmt->execute();
-    $results = $stmt->fetchAll();
-    usort($results, function ($a, $b) {
-        return $a['weight'] - $b['weight'];
-    });
+    $service = new Google_Service_YouTube($client);
 
-    $arrHtmlImages = [];
-    $arrHtmlImages2 = [];
-    
-    // Watch Later playlist.
-    $arrHtmlImages[-1] = "<div>Watch Later<br /><input type='text' id='' name='' value=''
-    	style='background-image: url(https://s.ytimg.com/yts/img/no_thumbnail-vfl4t3-4R.jpg);background-size: 100%;'
-    	onDrop='convert(event);' onPaste='convert(event);' /></div>";
-    
-    foreach ($playlistsResponse['items'] as $playlist) {
-        $tmp = "<div>{$playlist['snippet']['title']}<br /><input type='text' id='{$playlist['id']}' name='{$playlist['id']}' value=''
-        	style='background-image: url({$playlist['snippet']['thumbnails']['medium']['url']});background-size: 100%;'
-        	onDrop='convert(event);' onPaste='convert(event);' /></div>";
-        $key = array_search($playlist['id'], array_column($results, 'id'));
-        
-        if ($key !== false) {
-            $arrHtmlImages[$key] = $tmp;
-        } else {
-            $arrHtmlImages2[] = $tmp;
-        }
-        
-        if ($playlist['id'] === $playlistId) {
-            $strPlaylist = $playlist['snippet']['title'];
-        }
-    }
-    
-    ksort($arrHtmlImages);
-    
-    $htmlImages = implode($arrHtmlImages).implode($arrHtmlImages2);
-    
-    if ($videoIds !== '') {
-        try {
-            if ($playlistId === '') {
-                // Call the channels.list method to retrieve information about the currently authenticated user's channel.
-                $channelsResponse = $youtube->channels->listChannels('contentDetails', array(
-                    'mine' => 'true'
-                ));
-                
-                foreach ($channelsResponse['items'] as $channel) {
-                    // Extract the unique playlist ID that identifies the list of videos uploaded to the channel, and then call the playlistItems.list method to retrieve that list.
-                    $playlistId = $channel['contentDetails']['relatedPlaylists']['watchLater'];
-                }
+    $arrSubs = [
+        'UCo0U1tbk3YbqiLDhkeWOviQ' => 'Bob Lennon',
+        'UCY-_QmcW09PHAImgVnKxU2g' => 'SQUEEZIE GAMING',
+        'UCYGjxo5ifuhnmvhPvCc3DJQ' => 'Wankil Studio - Laink et Terracid',
+        'UC_yP2DpIgs5Y1uWC0T03Chw' => 'Joueur Du Grenier',
+        'UCow2IGnug1l3Xazkrc5jM_Q' => 'JOYCA',
+        'UCWeg2Pkate69NFdBeuRFTAw' => 'SQUEEZIE',
+        'UCZ_oIYI9ZNpOfWbpZxWNuRQ' => 'ZeratoR',
+        'UCU0FhLr6fr7U9GOn6OiQHpQ' => 'Officiel DEFAKATOR',
+        'UCsXVnXF2Lg8FZQZtxw5t2GA' => 'Mad Dog',
+        'UCV6uM3y-8TeO7JlCtcEN-Bg' => 'ScienceClic',
+		
+        'UCCMxHHciWRBBouzk-PGzmtQ' => 'Bazar du Grenier',
+        'UChFfLNTK64xQj7NscGmLLLg' => 'Cours de japonais !',
+        'UC-4M8AN08hw39nn2v91VuMQ' => 'CYRILmp4',
+        'UCSuUdvJ_ILNpxcC4kATuMvw' => 'imRodolphe - 80 Jours Japon',
+        'UCgvqvBoSHB1ctlyyhoHrGwQ' => 'Amixem',
+        'UCaNlbnghtwlsGF-KzAFThqA' => 'ScienceEtonnante',
+        'UCSzsmZdY_v3aYN-GDz26OoQ' => 'L\'Ermite Moderne',
+        'UCJIl08OJstq_9kgI9wf_xjg' => 'Groland Le Zapoï',
+		'UCnrhy622SNPt04Avj5idFIg' => 'Parole de chat',
+        'UC-4WUubuVGowG_R7gdgesPA' => 'Max Bird',
+		
+        'UCOchT7ZJ4TXe3stdLW1Sfxw' => 'Dans Ton Corps',
+        'UC1Ue7TuX3iH4y8-Qrjj-hyg' => 'J\'m\'énerve pas, j\'explique',
+        'UCgANFlYnbE47942AoazfNsg' => 'Lil Joyca',
+        'UCdHXMcWxwZr2qQpdCCOcSkw' => 'OLYDRI',
+        'UCcziTK2NKeWtWQ6kB5tmQ8Q' => 'e-penser',
+        'UCeR8BYZS7IHYjk_9Mh5JgkA' => 'Scilabus',
+        'UCDWnqQWk7e6rt-H-ZN6JKwg' => 'La Caverne de l\'Ermite Moderne',
+        'UCI0LNmSlhS-H9mGNPWM8gzQ' => 'At0mium',
+        'UC5Twj1Axp_-9HLsZ5o_cEQQ' => 'Doc Seven',
+        'UCNgudhx2s_ubmiA2L_2NMmg' => 'Defakator Vite Fait',
+		
+        'UCw3tZ7g_FljNjzGprCuptpA' => 'Taupe10',
+        'UCah8C0gmLkdtvsy0b2jrjrw' => 'Cyrus North',
+        'UCaZRpfXzlHVchIJMpf-PVFw' => 'Scilabus plus',
+		'UCq-8pBMM3I40QlrhM9ExXJQ' => 'La Tronche en Biais',
+		'UCMFcMhePnH4onVHt2-ItPZw' => 'Hygiène Mentale',
+		'UCOuIgj0CYCXCvjWywjDbauw' => 'Chat Sceptique',
+        'UC_56vSO35nctESDan8agevg' => 'DeBunKer des Etoiles',
+		'UCCelP6iJfYoj0Ea3qXqugXQ' => 'Mike Horn',
+		'UCqA8H22FwgBVcF3GJpp0MQw' => 'Monsieur Phi',
+        'UC9VMz-llpSHTIfOzuggf5zA' => 'Japania',
+		
+		'UCKwLqdUm-iOxVA2ERRTsFng' => 'Ichiban Japan',
+        'UCfwWXdYixiKsWa_VogacrGg' => 'Le Japon fou fou fou',
+		'UCeOp9CWBaW2tVIBAzCobzow' => 'L\'antisèche',
+		'UCtqICqGbPSbTN09K1_7VZ3Q' => 'DirtyBiology',
+        'UCYK1TyKyMxyDQU8c6zF8ltg' => 'Funny Pet Videos',
+		'UCZpJLCV3gVP8R_sc8P30VpQ' => 'BOTCH',
+		'UC2_OG1L8DLTzQ7UrZVOk7OA' => 'Axolotblog',
+		'UCAx1h7lSxq1SHkEB2LRGfDQ' => 'Nus & Culottés',
+        'UCLXDNUOO3EQ80VmD9nQBHPg' => 'Fouloscopie',
+		'UCj--qtjUrXbWABX-KWLRu9A' => 'Ludovic B',
+		
+        // 'UC3ajq73sIK0OPwoK7GuVISw' => 'EnfluredeRenard',
+		// 'UC1EacOJoqsKaYxaDomTCTEQ' => 'Le Réveilleur',
+		// 'UCk4xx9_ljrqPjT2tnk34k2g' => 'Enzo Sultan Mafia Tripes',
+		// 'UC0NCbj8CxzeCGIF6sODJ-7A' => 'Science4All',
+		// 'UCB1K5xp5SME58OR0hEW8w9A' => 'Curieux du Japon',
+        // 'UCLT3vrpSjt-NfCHsSAGJwJQ' => 'cotcotprod',
+        // 'UC30EA869VRGNMw2qFYIdX5g' => 'Anto80',
+		// 'UCS_7tplUgzJG4DhA16re5Yg' => 'Balade Mentale',
+        // 'UC8Ux-LOyEXeioYQ4LFzpBXw' => 'Aude WTFake',
+		
+        // 'UCWVmHgIv4U5Bpik88ZqyjGA' => 'JinnKid',
+		// 'UCkMsSGlEMPRxhqvGj6qeucQ' => '12 Parsecs',
+		// 'UCP4wIoy9W9WdAfVIN2sVmEw' => 'Skyrroz',
+		// 'UCGWoTWsXJi1UtCjOiBHIsfw' => 'Sofyan',
+        // 'UChV2oq_a-UZfJF-UiW0u-DQ' => 'MATH',
+        // 'UCyWqModMQlbIo8274Wh_ZsQ' => 'Cyprien',
+        // 'UCyIqcxz-vR_o2GK4HWuZL8w' => 'CatPusic',
+        // 'UCus9EeXDcLaCJhVXYd6PJcg' => 'mistermv',
+        // 'UCBuee5JCBlI_VuKjxmqtyUA' => 'Best Of Antoine Daniel',
+    ];
+
+    $strSubs = implode(",", array_keys($arrSubs));
+	// echo $strSubs;die;
+           
+    $queryParams = [
+       'id' => $strSubs
+    ];
+
+    $resChannels = $service->channels->listChannels('contentDetails', $queryParams);
+	// echo 'resChannels<br>';
+	// print_r($resChannels);
+	// die;
+
+    $arrVideos = [];
+
+	$arrFilter = [
+		"clip officiel",
+		"making of",
+		"en live",
+		"\- live",
+		"bazar du grenier \- critique",
+		"ermite réagit",
+		"détatouage laser",
+		"détatouage laser", // autre écriture
+		"flander's company",
+		"l'épopée temporelle",
+		"wtfake academy",
+		"sans filtre #",
+		"parole de deporte",
+	];
+	$strFilter = '/('.implode('|', $arrFilter).')/i';
+
+    foreach ($resChannels->items as $channel) {
+        $uploadsId = $channel->contentDetails->relatedPlaylists->uploads;
+        // get uploaded videos from a chan, sort by date descendant.
+        $queryParams = [
+            'playlistId' => $uploadsId,
+            'maxResults' => '50'
+        ];
+
+        $reslistPlaylistItems = $service->playlistItems->listPlaylistItems('snippet', $queryParams);
+		// echo 'reslistPlaylistItems<br>';
+        /*
+        Live 
+        en live 
+
+        best of:
+        zerator
+        moman
+        gius
+        mistermv 
+        */
+        foreach ($reslistPlaylistItems['items'] as $video) {
+            if (preg_match($strFilter, $video->snippet->title) !== 1) {
+                $arrVideos[$video->snippet->resourceId->videoId] = [
+                    'channelTitle' => $video->snippet->channelTitle,
+                    'title' => $video->snippet->title,
+                    'videoId' => $video->snippet->resourceId->videoId,
+                    'publishedAt' => $video->snippet->publishedAt,
+                    'publishedMonth' => substr($video->snippet->publishedAt, 0, 7),
+                    'isRated'  => 0,
+                    'isPlaylist'  => 0,
+                    'duration'  => '',
+                ];
             }
-            
-            // This code adds a video to the playlist. First, define the resource being added to the playlist by setting its video ID and kind.
-            $resourceId = new Google_Service_YouTube_ResourceId();
-            $resourceId->setVideoId($videoIds);
-            $resourceId->setKind('youtube#video');
-            
-            // Then define a snippet for the playlist item. Set the playlist item's title if you want to display a different value than the title of the video being added.
-            // Add the resource ID and the playlist ID retrieved in step 4 to the snippet as well.
-            $playlistItemSnippet = new Google_Service_YouTube_PlaylistItemSnippet();
-            $playlistItemSnippet->setTitle('First video in the test playlist');
-            $playlistItemSnippet->setPlaylistId($playlistId);
-            $playlistItemSnippet->setResourceId($resourceId);
-            
-            // Finally, create a playlistItem resource and add the snippet to the resource, then call the playlistItems.insert method to add the playlist item.
-            $playlistItem = new Google_Service_YouTube_PlaylistItem();
-            $playlistItem->setSnippet($playlistItemSnippet);
-            $playlistItemResponse = $youtube->playlistItems->insert('snippet,contentDetails', $playlistItem, array());
-            
-            $htmlBody .= "<p style='color:green'>Added to $strPlaylist:</p>";
-            $htmlBody .= sprintf('<p style="text-align: left;">%s (%s)</p>', $playlistItemResponse['snippet']['title'], $playlistItemResponse['id']);
-        } catch (Google_ServiceException $e) {
-            $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>', htmlspecialchars($e->getMessage()));
-        } catch (Google_Exception $e) {
-            if ($e->getErrors()[0]['reason'] == 'videoAlreadyInPlaylist') {
-                $htmlBody .= '<p style="color:orange">Video already added to Watch Later.</p>';
-            } else {
-                $htmlBody .= sprintf('<p style="color:red">A client error occurred: <code>%s</code></p>', htmlspecialchars($e->getMessage()));
+        }
+    }
+
+    $arrVideoIds = [];
+    $strVideos = '';
+    $i = 0;
+
+    foreach (array_keys($arrVideos) as $videoId) {
+        $strVideos .= $videoId.',';
+        $i++;
+
+        if ($i % 49 == 0) {
+            $arrVideoIds[] = $strVideos;
+            $strVideos = '';
+            $i = 0;
+        }
+    }
+
+    $arrVideoIds[] = $strVideos;
+
+    foreach ($arrVideoIds as $strVideos) {
+        $ratings = $service->videos->getRating($strVideos);
+		// echo 'ratings<br>';
+
+        foreach ($ratings->items as $rating) {
+            if ($rating->rating != 'none') {
+                $arrVideos[$rating->videoId]['isRated'] = 1;
             }
         }
-    } else {
-        $htmlBody = '<p style="color:purple">No video ID specified.</p>';
-    }
-    
-    if (isset($dbToken)) {
-        $sql = "UPDATE token SET token=:token WHERE type=:type";
-    } else {
-        $sql = "INSERT INTO token (token,type) VALUES (:token,:type)";
-    }
-    
-    $dbToken = $client->getAccessToken();
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
-        //':token' => $dbToken,
-        ':token' => json_encode($dbToken),
-        ':type' => 'token'
-    ));
-    //echo '<pre>';var_dump($client->getAccessToken());die;
-    
-    //if (isset(json_decode($client->getAccessToken())->refresh_token)) {
-    if (isset(($client->getAccessToken())->refresh_token)) {
-        if (isset($dbRefreshToken)) {
-            $sql = "UPDATE token SET token=:token WHERE type=:type";
-        } else {
-            $sql = "INSERT INTO token (token,type) VALUES (:token,:type)";
+
+        $queryParams = [
+            'id' => $strVideos
+        ];
+
+        $resVideos = $service->videos->listVideos('contentDetails', $queryParams);
+		// echo 'resVideos<br>';
+		
+        foreach ($resVideos->items as $video) {
+            $arrVideos[$video->id]['duration'] = covtime($video->contentDetails->duration);
+            //$arrVideos[$video->id]['publishedMonth'] .= '-'.(999 - sprintf('%03d', covtime($video->contentDetails->duration)));
+            $arrVideos[$video->id]['publishedMonth'] = str_replace('-', '', $arrVideos[$video->id]['publishedMonth']).(999 - sprintf('%03d', covtime($video->contentDetails->duration)));
         }
-        
-        $dbRefreshToken = json_decode($client->getAccessToken())->refresh_token;
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute(array(
-            ':token' => $dbRefreshToken,
-            ':type' => 'refreshtoken'
-        ));
-    } elseif (! isset($dbRefreshToken)) {
-        unset($dbToken);
-        
-        $stmt = $pdo->prepare("DELETE FROM token WHERE type=:type");
-        $stmt->execute(array(
-            ':type' => 'token'
-        ));
-        $client->revokeToken();
     }
+
+    $arrMyPlaylists = [
+        'prime' => 'PL52YqI0PbEYU1_3bne33bXQYAviFN8AD4',
+        'zera' => 'PL52YqI0PbEYX18AmDYyv1lx28lIzwYgTb',
+        'lennon' => 'PL52YqI0PbEYXMGfA71veOZCM_4NAWVopj',
+        'zap' => 'PL52YqI0PbEYV_g-5wdATX6GV7QPXvPadH',
+        'long' => 'PL52YqI0PbEYXZv2APe7B3wPkgdVXi1yK7',
+        // 'japon short' => 'PL52YqI0PbEYWYGVaDb7WaotZw1-DqeIbt',
+        'japon' => 'PL52YqI0PbEYXzJuYKJFyCCu1lREuoMhnO',
+        'podcast' => 'PL52YqI0PbEYUxtItqu6WAIqj1C7hX4LUL',
+		
+    ];
+
+    $strMyPlaylists = implode(',', $arrMyPlaylists);
+    $arrMyPlaylistItems = [];
+
+    foreach ($arrMyPlaylists as $myPlaylist) {
+        $pageToken = "";
+
+        while (!is_null($pageToken)) {
+            $queryParams = [
+                'maxResults' => 50,
+                'pageToken' => $pageToken,
+                'playlistId' => $myPlaylist
+            ];
+            
+            $myPlaylistItems = $service->playlistItems->listPlaylistItems('contentDetails', $queryParams);
+			// echo 'myPlaylistItems<br>';
+            $pageToken = $myPlaylistItems->nextPageToken;
+            $arrMyPlaylistItems[] = $myPlaylistItems;
+        }
+    }
+
+    foreach ($arrMyPlaylistItems as $myPlaylistItems) {
+        foreach ($myPlaylistItems as $myPlaylistItemDetails) {
+            $arrVideos[$myPlaylistItemDetails->contentDetails->videoId]['isPlaylist'] = 1;
+        }
+    }
+	
     
-    $htmlBody = "<div id='message'>$htmlBody</div>$htmlImages<br />";
+    usort($arrVideos, function($a, $b) {
+        if (isset($a['publishedMonth']) && isset($b['publishedMonth'])) {
+            return $b['publishedMonth'] <=> $a['publishedMonth'];
+        }
+    });
+	
+	//usort($arrVideos, "cmp");
+	
+    $strMonth = '';
+
+    foreach ($arrVideos as $videos) {
+        if (isset($videos['isRated']) && isset($videos['isPlaylist']) && $videos['isRated'] == 0 && $videos['isPlaylist'] == 0) {
+            if ($strMonth !== substr($videos['publishedMonth'], 0, 6)) {
+                $strMonth = substr($videos['publishedMonth'], 0, 6);
+                echo "<h1>$strMonth</h1>";
+            }
+
+            echo "{$videos['channelTitle']} - {$videos['title']}<br>";
+            echo "<a href='https://www.youtube.com/watch?v={$videos['videoId']}'>{$videos['videoId']}</a> - {$videos['publishedAt']} - {$videos['duration']} - {$videos['publishedMonth']}<hr>\r\n";
+        }
+    }
 } else {
     // If the user hasn't authorized the app, initiate the OAuth flow.
     $state = mt_rand();
@@ -267,71 +399,20 @@ if ($client->getAccessToken()) {
     $authUrl = $client->createAuthUrl();
     $htmlBody = "<p>Authorization Required</p><p>You need to <a href='$authUrl'>authorize access</a> before proceeding.<p>";
 }
-?>
 
-<!doctype html>
-<html>
-<head>
-<title>Add to Watch Later</title>
-<style>
-p, div {
-	font: 12px Arial;
-	/*width: 100%;*/
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	padding: 5px;
+/*
+function cmp($a, $b)
+{
+    return strcmp($a["publishedMonth"], $b["publishedMonth"]);
 }
+*/
 
-div {
-	width: 134px;
-	height: 50px;
-	float: left;
+function covtime($youtube_time){    
+    if($youtube_time) {
+        $start = new DateTime('@0'); // Unix epoch
+        $start->add(new DateInterval($youtube_time));
+        $youtube_time = round($start->getTimestamp() / 60, 0);
+    }
+    
+    return $youtube_time;
 }
-
-input, img {
-	width: 134px;
-	height: 40px;
-	border: 0;
-	background-color: #ddd;
-}
-
-#container, #message {
-	width: 100%;
-	height: 100%;
-}
-</style>
-</head>
-<body>
-	<script>
-        function convert(event) {
-            setTimeout(function () {
-                var val = decodeURIComponent(event.target.value);
-                var vid = val.split('v=')[1];
-                
-				if (typeof vid === "undefined") {
-					var vid = val.split('youtu.be/')[1];
-					
-					if (typeof vid === "undefined") {
-						var vid = val.split('video_ids=')[1];
-						vid = vid.split('%')[0];
-					}
-                }
-                
-                if (typeof vid === "undefined") {
-                    document.getElementById('message').innerHTML = '<p style="color:red">No video ID found in string.</p>';
-                } else {
-                    var andPosition = vid.indexOf('&');
-                    
-                    if (andPosition != -1) {
-                        vid = vid.substring(0, andPosition);
-                    }
-                    
-                    window.location.href = '//' + location.host + location.pathname + '?playlist_id=' + event.target.id + '&video_ids=' + vid
-                }
-            }, 500, event);
-        }
-    </script>
-	<div id='container'><?=$htmlBody?></div>
-</body>
-</html>
